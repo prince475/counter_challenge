82fa05395923f612443982060cf71e059908cb64
prince475 <danielmwai475@gmail.com>
2023-06-01 18:32:15 +0300

    A note

--- src/App.js
+++ src/App.js
@@ -6,6 +6,17 @@ function App() {
    * Challenge: set up state to track our count (initial value is zero)
    * see if you can add 1 to the count
    */
+
+  /**
+   * Note: if you need the old value of state
+   * to help you determine the new value of state,
+   * state setter function instead of using
+   * state directly. This callback function will
+   * receive the old value of state as its parameter,
+   * which you can then use to determine your new
+   * value of state.
+   */
+
   const [count, setCount] = useState(0)
   // one way of doing
 
